a95f51d20a80cc1db949652b84c4017f08c48538
VeselovDA <dimonwes@gmail.com>
2020-11-08 19:28:16 +0300

    add start generation

--- genericFindMax/MainWindow.xaml.cs
+++ genericFindMax/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,45 @@ namespace genericFindMax
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Generation> dgList = new ObservableCollection<Generation>();//коллекция для таблицы
+        int startRange;// начало диапозона
+        int endRange;// конец диапозона
+        int N;// переменная N(+ вариант)
         public MainWindow()
         {
             InitializeComponent();
+            dgGeneration.ItemsSource = dgList;
+            resetBtn_Click(resetBtn, null);
+
+        }
+
+        private void findMax_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void resetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                startRange = Convert.ToInt32(textStartRange.Text);
+                endRange= Convert.ToInt32(textEndRange.Text);
+                N= Convert.ToInt32(textN.Text);
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Некореектный ввод");
+            }
+            if (startRange > endRange)
+            {
+                startRange ^= endRange; endRange ^= startRange; startRange ^= endRange;// поменять местами переменные
+                MessageBox.Show("Переменные поменялись местами");
+            }
+            dgList.Clear();
+            //dgGeneration.ItemsSource.Cle;
+            Generation generation = new Generation(startRange, endRange);
+            dgList.Add(generation);
+
         }
     }
 }
